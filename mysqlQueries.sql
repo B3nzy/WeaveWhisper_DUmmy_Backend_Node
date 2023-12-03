1e08961bd669e492e865e7ee5a427f7c1fe14c1e
Sumit Mandal <sumit.mandal0123@gmail.com>
2023-12-03 12:33:28 +0530

    modify user table add type of the user to the sql query.

--- mysqlQueries.sql
+++ mysqlQueries.sql
@@ -3,7 +3,8 @@ CREATE TABLE user(
     name VARCHAR(100),
     email VARCHAR(50) UNIQUE, 
     password VARCHAR(50),
+    type VARCHAR(50),
     PRIMARY KEY(ID)
 );
 
-INSERT INTO user(name, email, password) VALUES("Sumit Mandal", "sumit.mandal0123@gmail.com", "sumit@123");
+INSERT INTO user(name, email, password, type) VALUES("Sumit Mandal", "sumit.mandal0123@gmail.com", "sumit@123", "customer");
